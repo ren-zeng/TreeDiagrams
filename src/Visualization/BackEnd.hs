754138b8ef5fafb9e75398bfb37a4d58f0e25f8b
zeng-ren <zeng.ren@epfl.ch>
2025-11-19 17:14:51 +0100

    reexport mkSizeSpec2D

--- src/Visualization/BackEnd.hs
+++ src/Visualization/BackEnd.hs
@@ -1,10 +1,14 @@
 module Visualization.BackEnd (
     BackEnd,
     renderSVG,
+    mkSizeSpec2D,
+    writeSVG,
     ) where
 
 import Diagrams.Backend.SVG (B, renderSVG)
+import Diagrams (mkSizeSpec2D)
 
 
 type BackEnd = B
 
+writeSVG path = renderSVG path (mkSizeSpec2D (Just 1000) (Just 1000) )
